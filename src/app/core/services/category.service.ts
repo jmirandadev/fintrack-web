b0647af476712f3db6fc70fed35d71fe44cf5dba
FinTrack Dev <fintrack@dev.com>
2026-06-07 23:02:14 -0500

    fix category service URL

--- src/app/core/services/category.service.ts
+++ src/app/core/services/category.service.ts
@@ -6,7 +6,7 @@ import { environment } from '../../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class CategoryService {
-  private readonly API = `${environment.apiUrl}/auth`;
+  private readonly API = `${environment.apiUrl}/categories`;
 
   constructor(private http: HttpClient) {}
 
